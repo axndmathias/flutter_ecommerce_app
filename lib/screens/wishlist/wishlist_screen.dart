8ef37500aaa18c36fe73c9c8c8c6a6693928ec3b
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-08-27 22:31:59 +0000

    added wishlist screen

---
+++ lib/screens/wishlist/wishlist_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/widgets/widgets.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({Key? key}) : super(key: key);
+
+  static const String routeName = '/wishlist';
+
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const WishlistScreen(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Wishlist'),
+      bottomNavigationBar: CustomNavBar(),
+    );
+  }
+}
