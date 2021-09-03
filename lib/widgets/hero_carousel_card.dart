ac9812a950be9ea54199e7ed44ef2bd6112d83a7
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-09-03 23:38:59 +0200

    created hero carousel card widget

---
+++ lib/widgets/hero_carousel_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/models/models.dart';
+
+class HeroCarouselCard extends StatelessWidget {
+  final Category category;
+
+  const HeroCarouselCard({
+    required this.category,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 20.0),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(
+          children: <Widget>[
+            Image.network(
+              category.imageUrl,
+              fit: BoxFit.cover,
+              width: 1000.0,
+            ),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(200, 0, 0, 0),
+                      Color.fromARGB(0, 0, 0, 0)
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 20.0,
+                ),
+                child: Text(
+                  category.name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline2!
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'custom_appbar.dart';
 export 'custom_navbar.dart';
+export 'hero_carousel_card.dart';
