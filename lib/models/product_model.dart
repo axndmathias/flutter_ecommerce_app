736ed6bf0cf6f05474adc35bbab009d140feb15c
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-09-08 21:23:23 +0000

    added product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,77 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String name;
+  final String category;
+  final String imageUrl;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
+
+  const Product({
+    required this.name,
+    required this.category,
+    required this.imageUrl,
+    required this.price,
+    required this.isRecommended,
+    required this.isPopular,
+  });
+
+  @override
+  List<Object?> get props => [
+        name,
+        category,
+        imageUrl,
+        price,
+        isPopular,
+        isRecommended,
+      ];
+
+  static List<Product> products = [
+    const Product(
+      name: 'SOft Drink #1',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'SOft Drink #2',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'SOft Drink #3',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Smoothies #1',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Smoothies #2',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: false,
+      isPopular: false,
+    ),
+  ];
+}
