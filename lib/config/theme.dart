b39017cc2320375f382cd7ff335896a26e56f09c
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-09-12 06:07:37 +0000

    issue resolved with theme font does not work

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
+    fontFamily: "Muli",
     textTheme: textTheme(),
   );
 }
