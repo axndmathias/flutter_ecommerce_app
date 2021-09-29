9751dcad789b9f912276b6ab7b75c2ba524639a0
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-09-29 04:12:13 +0000

    added a firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/blocs/cart/cart_bloc.dart';
@@ -8,7 +9,10 @@ import 'package:flutter_ecommerce_app/simple_bloc_observer.dart';
 
 import 'screens/screens.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
