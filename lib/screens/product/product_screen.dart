1654f7495ddd3ab57a0aa63b2afd31a55681e16e
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-10-03 05:35:52 +0000

    updated custom nav bar product

--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: use_key_in_widget_constructors
+
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_ecommerce_app/blocs/wishlist/wishlist_bloc.dart';
 import 'package:flutter_ecommerce_app/models/product_model.dart';
 import 'package:flutter_ecommerce_app/widgets/widgets.dart';
 
@@ -12,151 +10,109 @@ class ProductScreen extends StatelessWidget {
 
   static Route route({required Product product}) {
     return MaterialPageRoute(
-      // ignore: prefer_const_constructors
-      settings: RouteSettings(name: routeName),
+      settings: const RouteSettings(name: routeName),
       builder: (context) => ProductScreen(product: product),
     );
   }
 
   final Product product;
 
-  const ProductScreen({required this.product});
+  // ignore: use_key_in_widget_constructors
+  const ProductScreen({
+    required this.product,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: product.name),
-
-      // bottom nab bar
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.black,
-        child: SizedBox(
-          height: 70,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.share,
-                    color: Colors.white,
-                  )),
-              BlocBuilder<WishlistBloc, WishlistState>(
-                builder: (context, state) {
-                  return IconButton(
-                      onPressed: () {
-                        context
-                            .read<WishlistBloc>()
-                            .add(AddWishlistProduct(product));
-
-                            final snackBar = SnackBar(content: Text('Added to your Wishlist!'));
-                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      },
-                      icon: const Icon(
-                        Icons.favorite,
-                        color: Colors.white,
-                      ));
-                },
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.white),
-                onPressed: () {},
-                child: Text('ADD TO CART',
-                    style: Theme.of(context).textTheme.headline3!),
-              ),
-            ],
-          ),
-        ),
-      ),
+      bottomNavigationBar: CustomNavBar(screen: routeName, product: product),
       body: ListView(
         children: [
           CarouselSlider(
             options: CarouselOptions(
-              aspectRatio: 2.0,
+              aspectRatio: 1.5,
               viewportFraction: 0.9,
               enlargeCenterPage: true,
               enlargeStrategy: CenterPageEnlargeStrategy.height,
             ),
-            items: [
-              HeroCarouselCard(
-                product: product,
-              ),
-            ],
+            items: [HeroCarouselCard(product: product)],
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Stack(
+            child: Column(
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 60,
-                  alignment: Alignment.bottomCenter,
-                  color: Colors.black.withAlpha(50),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(5.0),
-                  width: MediaQuery.of(context).size.width - 10,
-                  height: 50,
-                  alignment: Alignment.bottomCenter,
-                  color: Colors.black,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          product.name,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline5!
-                              .copyWith(color: Colors.white),
-                        ),
-                        Text(
-                          '${product.price}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline5!
-                              .copyWith(color: Colors.white),
-                        ),
-                      ],
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      alignment: Alignment.bottomCenter,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withAlpha(50),
+                      ),
                     ),
+                    Container(
+                      margin: const EdgeInsets.all(5.0),
+                      width: MediaQuery.of(context).size.width - 10,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              product.name,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline5!
+                                  .copyWith(color: Colors.white),
+                            ),
+                            Text(
+                              '\$${product.price}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline5!
+                                  .copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                ExpansionTile(
+                  initiallyExpanded: true,
+                  title: Text(
+                    "Product Information",
+                    style: Theme.of(context).textTheme.headline3,
                   ),
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                        'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ],
                 ),
-              ],
-            ),
-          ),
-          // product information
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: ExpansionTile(
-              initiallyExpanded: true,
-              title: Text('Product Information',
-                  style: Theme.of(context).textTheme.headline3),
-              children: [
-                ListTile(
+                ExpansionTile(
                   title: Text(
-                      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
-                      style: Theme.of(context).textTheme.bodyText1),
-                ),
-              ],
-            ),
-          ),
-          // delivery information
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: ExpansionTile(
-              initiallyExpanded: true,
-              title: Text('Delivery Information',
-                  style: Theme.of(context).textTheme.headline3),
-              children: [
-                ListTile(
-                  title: Text(
-                      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
-                      style: Theme.of(context).textTheme.bodyText1),
+                    "Delivery Information",
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                        'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ],
                 ),
               ],
             ),
--- lib/widgets/custom_navbar.dart
+++ lib/widgets/custom_navbar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_app/models/product_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_app/blocs/cart/cart_bloc.dart';
+import 'package:flutter_ecommerce_app/blocs/wishlist/wishlist_bloc.dart';
+import 'package:flutter_ecommerce_app/models/models.dart';
 
 class CustomNavBar extends StatelessWidget {
   final String screen;
@@ -16,31 +19,111 @@ class CustomNavBar extends StatelessWidget {
     return BottomAppBar(
       color: Colors.black,
       child: SizedBox(
-        height: 70.0,
+        height: 70,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/');
-              },
-              icon: const Icon(Icons.home, color: Colors.white),
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/cart');
-              },
-              icon: const Icon(Icons.shopping_cart, color: Colors.white),
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/user');
-              },
-              icon: const Icon(Icons.person, color: Colors.white),
-            ),
-          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: _selectNavBar(context, screen)!,
         ),
       ),
     );
   }
+
+  List<Widget>? _selectNavBar(context, screen) {
+    switch (screen) {
+      case '/':
+        return _buildNavBar(context);
+      case '/catalog':
+        return _buildNavBar(context);
+      case '/wishlist':
+        return _buildNavBar(context);
+      case '/product':
+        return _buildAddToCartNavBar(context, product);
+      // case '/cart':
+      //   return _buildGoToCheckoutNavBar(context);
+      // case '/checkout':
+      //   return _buildOrderNowNavBar(context);
+
+      default:
+        _buildNavBar(context);
+    }
+  }
+
+// build nav bar
+  List<Widget> _buildNavBar(context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.home, color: Colors.white),
+        onPressed: () {
+          Navigator.pushNamed(context, '/');
+        },
+      ),
+      IconButton(
+        icon: const Icon(Icons.shopping_cart, color: Colors.white),
+        onPressed: () {
+          Navigator.pushNamed(context, '/cart');
+        },
+      ),
+      IconButton(
+        icon: const Icon(Icons.person, color: Colors.white),
+        onPressed: () {
+          Navigator.pushNamed(context, '/user');
+        },
+      )
+    ];
+  }
 }
+
+// 
+ // ignore: unused_element
+ List<Widget> _buildAddToCartNavBar(context, product) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.share, color: Colors.white),
+        onPressed: () {},
+      ),
+      BlocBuilder<WishlistBloc, WishlistState>(
+        builder: (context, state) {
+          if (state is WishlistLoading) {
+            return const CircularProgressIndicator();
+          }
+          if (state is WishlistLoaded) {
+            return IconButton(
+              icon: const Icon(Icons.favorite, color: Colors.white),
+              onPressed: () {
+                final snackBar =
+                    SnackBar(content: Text('Added to your Wishlist!'));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                context.read<WishlistBloc>().add(AddWishlistProduct(product));
+              },
+            );
+          }
+          return const Text('Something went wrong!');
+        },
+      ),
+      BlocBuilder<CartBloc, CartState>(
+        builder: (context, state) {
+          if (state is CartLoading) {
+            return const CircularProgressIndicator();
+          }
+          if (state is CartLoaded) {
+            return ElevatedButton(
+              onPressed: () {
+                context.read<CartBloc>().add(CartProductAdded(product));
+                Navigator.pushNamed(context, '/cart');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                shape: const RoundedRectangleBorder(),
+              ),
+              child: Text(
+                'ADD TO CART',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+            );
+          }
+          return const Text('Something went wrong!');
+        },
+      )
+    ];
+  }
